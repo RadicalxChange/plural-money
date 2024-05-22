f7e7e1bef7c01237090629ff1f343affd52ac4de
Alex Randaccio <acrandaccio@gmail.com>
2024-05-22 14:44:27 -0400

    move zuauth logic to signIn page

--- components/googleSignInButton.tsx
+++
@@ -1,8 +0,0 @@
-export default function GoogleSignInButton() {
-
-    return (
-        <button className="w-full px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-black bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            <a href="/api/auth/login">Sign In With Google</a>
-        </button>
-    );
-  }
