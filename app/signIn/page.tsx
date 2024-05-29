296150cdbcf9a38660552db4bc77b4b86ec377eb
Alex Randaccio <acrandaccio@gmail.com>
2024-05-29 14:56:26 -0400

    temporarily remove zupass

--- app/signIn/page.tsx
+++ app/signIn/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { config } from "@/config/zuauth";
-import { redirectHome } from "@/lib/redirectHome";
-import { zuAuthPopup } from "@pcd/zuauth/client";
+// import { config } from "@/config/zuauth";
+// import { redirectHome } from "@/lib/redirectHome";
+// import { zuAuthPopup } from "@pcd/zuauth/client";
 import { useCallback, useEffect, useState } from "react";
 
 type AuthState =
@@ -15,58 +15,58 @@ type AuthState =
 export default function SignIn() {
   const [authState, setAuthState] = useState<AuthState>("logged out");
 
-  useEffect(() => {
-    (async () => {
-      if (authState === "auth-start") {
-        console.log("Fetching watermark");
-        const watermark = (await (await fetch("/api/auth/zupass/watermark")).json())
-          .watermark;
-        console.log("Got watermark");
-        console.log("Opening popup window");
-        setAuthState("authenticating");
-        const result = await zuAuthPopup({
-          zupassUrl: process.env.NEXT_PUBLIC_ZUPASS_SERVER_URL as string,
-          fieldsToReveal: {
-            revealAttendeeEmail: true,
-            revealAttendeeName: true,
-            revealProductId: false
-          },
-          watermark,
-          config: config
-        });
+  // useEffect(() => {
+  //   (async () => {
+  //     if (authState === "auth-start") {
+  //       console.log("Fetching watermark");
+  //       const watermark = (await (await fetch("/api/auth/zupass/watermark")).json())
+  //         .watermark;
+  //       console.log("Got watermark");
+  //       console.log("Opening popup window");
+  //       setAuthState("authenticating");
+  //       const result = await zuAuthPopup({
+  //         zupassUrl: process.env.NEXT_PUBLIC_ZUPASS_SERVER_URL as string,
+  //         fieldsToReveal: {
+  //           revealAttendeeEmail: true,
+  //           revealAttendeeName: true,
+  //           revealProductId: false
+  //         },
+  //         watermark,
+  //         config: config
+  //       });
 
-        if (result.type === "pcd") {
-          console.log("Received PCD");
+  //       if (result.type === "pcd") {
+  //         console.log("Received PCD");
 
-          const loginResult = await fetch("/api/auth/zupass/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ pcd: result.pcdStr })
-          });
+  //         const loginResult = await fetch("/api/auth/zupass/login", {
+  //           method: "POST",
+  //           headers: { "Content-Type": "application/json" },
+  //           body: JSON.stringify({ pcd: result.pcdStr })
+  //         });
 
-          console.log("Authenticated successfully");
-          setAuthState("authenticated");
-          redirectHome();
-        } else if (result.type === "popupBlocked") {
-          console.log("The popup was blocked by your browser");
-          setAuthState("error");
-        } else if (result.type === "popupClosed") {
-          console.log("The popup was closed before a result was received");
-          setAuthState("error");
-        } else {
-          console.log(`Unexpected result type from zuAuth: ${result.type}`);
-          setAuthState("error");
-        }
-      }
-    })();
-  }, [authState]);
+  //         console.log("Authenticated successfully");
+  //         setAuthState("authenticated");
+  //         redirectHome();
+  //       } else if (result.type === "popupBlocked") {
+  //         console.log("The popup was blocked by your browser");
+  //         setAuthState("error");
+  //       } else if (result.type === "popupClosed") {
+  //         console.log("The popup was closed before a result was received");
+  //         setAuthState("error");
+  //       } else {
+  //         console.log(`Unexpected result type from zuAuth: ${result.type}`);
+  //         setAuthState("error");
+  //       }
+  //     }
+  //   })();
+  // }, [authState]);
 
-  const auth = useCallback(() => {
-    if (authState === "logged out" || authState === "error") {
-      console.log("Beginning authentication");
-      setAuthState("auth-start");
-    }
-  }, [authState]);
+  // const auth = useCallback(() => {
+  //   if (authState === "logged out" || authState === "error") {
+  //     console.log("Beginning authentication");
+  //     setAuthState("auth-start");
+  //   }
+  // }, [authState]);
 
   return (
     <main className="flex flex-col items-center justify-center border-b-2 border-white">
@@ -81,7 +81,7 @@ export default function SignIn() {
           >
             <a href="/api/auth/login">Sign In With Google</a>
           </button>
-          <button
+          {/* <button
             onClick={auth}
             className="block text-center px-6 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-black bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             disabled={
@@ -89,7 +89,7 @@ export default function SignIn() {
             }
           >
             Sign In With Zupass
-          </button>
+          </button> */}
         </div>
       </div>
     </main>
