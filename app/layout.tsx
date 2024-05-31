a337ae3d3c0b9cf39b940966094a7538eef8fd5c
Alex Randaccio <acrandaccio@gmail.com>
2024-05-31 17:19:06 -0400

    config metadata w icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Edge Esmeralda L∈dger",
   description: "A ledger for ∈dge holders",
+  metadataBase: new URL(process.env.AUTH0_BASE_URL || "http://localhost:3000"),
+  icons: {
+    icon: '/icon.svg',
+  },
+  openGraph: {
+    images: '/opengraph-image.png',
+  },
 };
 
 export default function RootLayout({
