e8c31fd2e8f14f1a695e1aeebac7e9b420d0d638
Alex Randaccio <acrandaccio@gmail.com>
2024-06-05 12:58:28 -0700

    sort participants alphabetically

--- app/participants/page.tsx
+++ app/participants/page.tsx
@@ -14,7 +14,13 @@ export default async function Participants() {
       <h1 className="text-left text-lg mb-12">{`{ ∈dge holders }`}</h1>
       {user && user.account_is_member ? (
         <ul className="w-full max-w-5xl font-mono lg:text-sm text-xs border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit rounded-xl border bg-gray-200 lg:p-4">
-          {accounts.map((account, index) => {
+          {accounts.sort((a: Account, b: Account) => {
+            let x = a.name.toLowerCase();
+            let y = b.name.toLowerCase();
+            if (x < y) {return -1;}
+            if (x > y) {return 1;}
+            return 0;
+          }).map((account, index) => {
             return (
               <li key={index} className={`px-4 py-2 my-2 flex justify-between items-center ${account.id === user.account_id ? 'text-blue-400' : ''}`}>
                   {account.name} <span className="font-bold">{account.balance + " " + (account.is_member ? "∈" : "∉")}</span>
