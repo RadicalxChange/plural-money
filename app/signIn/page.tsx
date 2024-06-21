bf44113c9c03037471b2a6988b7eae76d19891c4
Alex Randaccio <acrandaccio@gmail.com>
2024-06-21 17:46:28 -0400

    componentize signIn logic

--- app/signIn/page.tsx
+++ app/signIn/page.tsx
@@ -1,16 +1,4 @@
-"use client";
-
-import { config } from "@/config/zuauth";
-import { redirectHandler } from "@/lib/redirectHandler";
-import { zuAuthPopup } from "@pcd/zuauth/client";
-import { useCallback, useEffect, useState } from "react";
-
-type AuthState =
-  | "logged out"
-  | "auth-start"
-  | "authenticating"
-  | "authenticated"
-  | "error";
+import SignInButtons from "@/components/signInButtons";
 
 interface SignInProps {
   searchParams: {
@@ -19,85 +7,11 @@ interface SignInProps {
 }
 
 export default function SignIn({ searchParams }: SignInProps) {
-  const [authState, setAuthState] = useState<AuthState>("logged out");
-  var redirectUri: string = searchParams.redirectUri ?? '/'
-  redirectUri = decodeURIComponent(redirectUri)
-
-  useEffect(() => {
-    (async () => {
-      if (authState === "auth-start") {
-        console.log("Fetching watermark");
-        const watermark = (await (await fetch("/api/auth/zupass/watermark")).json())
-          .watermark;
-        console.log("Got watermark");
-        console.log("Opening popup window");
-        setAuthState("authenticating");
-        const result = await zuAuthPopup({
-          zupassUrl: process.env.NEXT_PUBLIC_ZUPASS_SERVER_URL as string,
-          fieldsToReveal: {
-            revealAttendeeEmail: true,
-            revealAttendeeName: true,
-            revealProductId: false
-          },
-          watermark,
-          config: config
-        });
-
-        if (result.type === "pcd") {
-          console.log("Received PCD");
-
-          const loginResult = await fetch("/api/auth/zupass/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ pcd: result.pcdStr })
-          });
-
-          console.log("Authenticated successfully");
-          setAuthState("authenticated");
-          redirectHandler(redirectUri);
-        } else if (result.type === "popupBlocked") {
-          console.log("The popup was blocked by your browser");
-          setAuthState("error");
-        } else if (result.type === "popupClosed") {
-          console.log("The popup was closed before a result was received");
-          setAuthState("error");
-        } else {
-          console.log(`Unexpected result type from zuAuth: ${result.type}`);
-          setAuthState("error");
-        }
-      }
-    })();
-  }, [authState]);
-
-  const auth = useCallback(() => {
-    if (authState === "logged out" || authState === "error") {
-      console.log("Beginning authentication");
-      setAuthState("auth-start");
-    }
-  }, [authState]);
 
   return (
     <main className="flex flex-col items-center justify-center w-full min-h-screen-minus-header bg-gradient-to-b">
       <h1 className="text-lg mb-6">Sign In</h1>
-      <div className="space-y-4 mb-12">
-        <button 
-          className="w-full px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-black bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          disabled={
-            authState === "auth-start" || authState === "authenticating"
-          }
-        >
-          <a href={`/api/auth/login?returnTo=${redirectUri}`}>Sign In With Google</a>
-        </button>
-        <button
-          onClick={auth}
-          className="block text-center px-6 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-black bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          disabled={
-            authState === "auth-start" || authState === "authenticating"
-          }
-        >
-          Sign In With Zupass
-        </button>
-      </div>
+      <SignInButtons redirectUri={searchParams.redirectUri ?? '/'} />
     </main>
   );
 }
